c4595c634f20e2e7923d0129ab1062c1422d62fe
Ed Campbell <drescampbell@gmail.com>
2017-02-22 16:25:15 +0000

    moved to client connection to handle error

--- scales.py
+++ scales.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
+import socket
 import time
 
 from hx711 import HX711
 from oled import OledDisplay
 import paho.mqtt.publish as publish
-
+import paho.mqtt.client as mqtt
 
 # Raspberry Pi pin configuration:
 # HX711 pins
@@ -26,13 +27,24 @@ def main():
 
     disp = OledDisplay()
 
+    client = mqtt.Client()
+    try:
+	client.connect(MQTT_HOSTNAME)
+    except socket.gaierror as e:
+        print('Cannot connect to MQTT broker: {}'.format(e))
+        client = None
+    else:
+        client.loop_start()  # context mgr is an option here
+
     while True:
         try:
             val = hx.get_weight(5)
 
             msg = '{:.1f} kg'.format(val)
             disp.display(msg)
-            publish.single(MQTT_TOPIC, msg, hostname=MQTT_HOSTNAME)
+            #publish.single(MQTT_TOPIC, val, hostname=MQTT_HOSTNAME)
+            if client is not None:
+                client.publish(MQTT_TOPIC, val)
             print(val)
             hx.power_down()
             hx.power_up()
